aa63bb2c5ff73432a49ce99d696ea102a01c9516
bichurinet <bichurinet@ya.ru>
2026-04-19 12:18:46 +0500

    update cfg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 // eslint-disable-next-line no-undef
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/',
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: `${process.env.NUXT_APP_BASE_URL || '/'}favicon.ico`,
+        },
+      ],
+    },
+  },
 })
