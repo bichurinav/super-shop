fe696e30cd416d74d582d2e0fcc121dd34b42e35
bichurinet <bichurinet@ya.ru>
2026-04-19 12:09:38 +0500

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line no-undef
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
